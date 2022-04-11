e2c0bf1520e52325e251df32c2654d305171331a
clsung <clsung7911@gmail.com>
2022-04-11 13:48:05 +0900

    include

--- demo/src/main/resources/static/scripts/map/fn_main.js
+++ demo/src/main/resources/static/scripts/map/fn_main.js
@@ -14,5 +14,22 @@
 2022-04-11 이성준 최초작성
 ********************************************/
 document.addEventListener("DOMContentLoaded", () => {
-
+	const vworldImageUrl = 'http://xdworld.vworld.kr:8080/2d/Base/201612/{z}/{x}/{y}.png';
+	let map = new ol.Map({
+		target: 'map',
+		layers: [
+			new ol.layer.Tile({
+				source: new ol.source.XYZ({
+					url: 'http://xdworld.vworld.kr:8080/2d/Base/202002/{z}/{x}/{y}.png'
+				})
+			})
+		],
+		view: new ol.View({
+			center: [14233127.961095788, 4321508.791022108],
+			zoom: 8,
+			minZoom: 8,
+			maxZoom: 19
+		}),
+		controls: []
+	});
 });
