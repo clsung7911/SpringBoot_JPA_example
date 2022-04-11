2f8df9ca3f88c942705c6b6227a60378bfb446d9
clsung <clsung7911@gmail.com>
2022-04-11 13:25:15 +0900

    script add

---
+++ demo/src/main/resources/static/scripts/map/fn_main.js
@@ -0,0 +1,18 @@
+/**
+ *   	Javascript Name  : fn_main.js
+			Description      : HereIt 지도서비스 지도화면 관리
+			Modification Information
+ */
+
+/********************************************
+함수명 : fn_main
+설 명 : HereIt 지도서비스 지도화면 관리
+작성일 : 2022-04-11
+작성자 : 이성준
+수정일 수정자 수정내용
+---------- ------ --------------
+2022-04-11 이성준 최초작성
+********************************************/
+document.addEventListener("DOMContentLoaded", () => {
+
+});
