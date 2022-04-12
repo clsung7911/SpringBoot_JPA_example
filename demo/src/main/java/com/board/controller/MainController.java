a1f2090dfbd7a6a0528d01a2f6e3f549a8d7fc0a
clsung <clsung7911@gmail.com>
2022-04-12 17:24:43 +0900

    login / join commit

--- demo/src/main/java/com/board/controller/MainController.java
+++ demo/src/main/java/com/board/controller/MainController.java
@@ -6,8 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+	/**
+	 * 로그인 페이지로 이동
+	 * @return
+	 */
 	@GetMapping("/")
 	public String main() {
 		return "login/login";
+	}
+	
+	/**
+	 * 회원가입 페이지 이동
+	 */
+	@GetMapping("/join")
+	public String join() {
+		return "login/join";
 	}
 }
